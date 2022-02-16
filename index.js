d99ce26280dc0b6560026bf4956f23e5c4a55735
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-16 15:53:15 +0100

    collecting numbers via optiens feature

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-//index
+require("dotenv").config();
+const debug = require("debug")("calc:");
+const { program } = require("commander");
+
+program.requiredOption("-n, --numbers <number...>");
+program.parse(process.argv);
+const options = program.opts();
+debug(options.numbers);
